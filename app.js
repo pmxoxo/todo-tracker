09d1d640e7069b35deba4a4beb9ca94cd04db33f
cafeobscura <pmxoxo@skiff.com>
2023-12-01 20:32:10 -0500

    Added a delete button to remove unwanted tasks from the task list.

--- app.js
+++ app.js
@@ -1,44 +1,50 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     function newTask() {
         var taskValue = document.getElementById('newtask').value;
 
         // Checks to see if input field is empty
         if (taskValue === '') {
-            alert("Task cannot be empty")
+            alert("Task cannot be empty");
             return;
         }
 
-
-        
-        // Creating a new list item 
+        // Create a new list item for each task
         var listItem = document.createElement('li');
 
+        // Text Node to store the input of the task name
+        var taskText = document.createTextNode(taskValue);
 
-
-        var taskText = document.createTextNode('taskValue');
-
-
-
+        // Checkbox to mark completed tasks
         var checkButton = document.createElement('input');
         checkButton.type = 'checkbox';
 
+        // Delete button to remove unwanted tasks
+        var deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete'; // Set text content for delete button
 
+        // Append elements to the new list item
         listItem.appendChild(taskText);
         listItem.appendChild(checkButton);
+        listItem.appendChild(deleteButton);
 
-        
+        // Append the new list item to the task list
         document.getElementById('taskList').appendChild(listItem);
 
 
+
+        // Event Listeners for Checkbox & Delete Buttons
         checkButton.addEventListener('click', function() {
             listItem.classList.toggle('completed');
         });
 
 
-        // Clear input field after adding new task 
+        deleteButton.addEventListener('click', function() {
+            listItem.remove();
+        });
+
+        // Clear input field after adding a new task 
         document.getElementById('newtask').value = '';
     }
+
     document.getElementById('addTaskButton').addEventListener('click', newTask);
 });
-
