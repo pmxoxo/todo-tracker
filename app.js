434bdda159cf65be35f54fb8d120f1b1b07cfb58
cafeobscura <pmxoxo@skiff.com>
2023-12-01 19:40:48 -0500

    finished 'add task' feature, which appends an <li> tag to the current list of tasks to complete

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ document.addEventListener('DOMContentLoaded', function() {
     function newTask() {
         var taskValue = document.getElementById('newtask').value;
 
+        // Checks to see if input field is empty
         if (taskValue === '') {
             alert("Task cannot be empty")
             return;
         }
         
-        console.log("Hello " + taskValue);  
+        // Creating a new list item 
+        var listItem = document.createElement('li');
+        listItem.textContent = taskValue;
         
+        document.getElementById('taskList').appendChild(listItem);
+
+        // Clear input field after adding new task 
         document.getElementById('newtask').value = '';
     }
     document.getElementById('addTaskButton').addEventListener('click', newTask);
