419689becfa7cb6140a8cdfcebfc03fe52634a4c
cafeobscura <pmxoxo@skiff.com>
2023-12-01 19:57:38 -0500

    Added a checkbox to each new task. Each task will toggle to 'completed' class when finished, which will turn the text green and strike through the item to show it has been completed

--- app.js
+++ app.js
@@ -8,12 +8,33 @@ document.addEventListener('DOMContentLoaded', function() {
             alert("Task cannot be empty")
             return;
         }
+
+
         
         // Creating a new list item 
         var listItem = document.createElement('li');
-        listItem.textContent = taskValue;
+
+
+
+        var taskText = document.createTextNode('taskValue');
+
+
+
+        var checkButton = document.createElement('input');
+        checkButton.type = 'checkbox';
+
+
+        listItem.appendChild(taskText);
+        listItem.appendChild(checkButton);
+
         
         document.getElementById('taskList').appendChild(listItem);
+
+
+        checkButton.addEventListener('click', function() {
+            listItem.classList.toggle('completed');
+        });
+
 
         // Clear input field after adding new task 
         document.getElementById('newtask').value = '';
